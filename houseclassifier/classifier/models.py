d8201ba17d0641a6e3be981b39020dbbdd6c4d4c
Manoharborana4030 <19mca050@nirmauni.ac.in>
2022-05-17 19:08:34 +0530

    second commit

--- houseclassifier/classifier/models.py
+++ houseclassifier/classifier/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Pre_Image(models.Model):
+    img_link=models.CharField(max_length=500)
+    cat_img=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.cat_img
--- houseclassifier/classifier/views.py
+++ houseclassifier/classifier/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
+from .Predicator import ImagePredict
+from .models import *
+
+obj=ImagePredict()
 
 def home(request):
 
@@ -10,5 +15,23 @@ def predicate(request):
     if request.method=='POST':
         file_name=request.FILES['file']
         print('@@@',file_name)
-
+        filename_new=file_name.name
+        filename_new=filename_new.replace(" ","_")
+        fs=FileSystemStorage()
+        fs.save(filename_new,file_name)
+        data=obj.predictor(filename_new)
+        temp=None
+        if data == 1:
+            temp='House Exterior'
+        if data == 2:
+            temp='Living Room'
+        if data == 3:
+            temp='Bed Room'
+        if data == 4:
+            temp='Washroom'
+        if data == 0:
+            temp='Kitchen'
+        s_obj=Pre_Image(img_link=f'media/{filename_new}',cat_img=temp)
+        s_obj.save()
+        return render(request,'result.html',{'data':temp})
     return render(request,'predicate.html')
